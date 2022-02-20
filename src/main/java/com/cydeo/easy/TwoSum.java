cb0111aa13bbe9aae5ee1bbe38fd59910a24736d
makhmudakhmedov34 <akhmedov_makhmud@yahoo.com>
2022-02-20 17:17:44 -0500

    two sum second solution

--- src/main/java/com/cydeo/easy/TwoSum.java
+++ src/main/java/com/cydeo/easy/TwoSum.java
@@ -1,25 +1,49 @@
 package com.cydeo.easy;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class TwoSum {
 
     public static void main(String[] args) {
-        int[] myarray = {2, 7, 9, 11};
+        int[] myArray = {2, 7, 9, 11};
+        int target = 9;
+        System.out.println("Two sum first solution");
+        System.out.println(Arrays.toString(twoSumSolution1(myArray,target)));
+        System.out.println("Two sum second solution");
+        System.out.println(Arrays.toString(twoSumSolution2(myArray,target)));
 
     }
 
     // solution one
-    public int[] twoSumSolution1(int[] array,int target){
+    public static int[] twoSumSolution1(int[] array,int target){
 
         Map<Integer, Integer> map = new HashMap<>();
 
         for(int i = 0; i < array.length; i++){
             int potentialMatch = target - array[i];
-            if(potentialMatch==array[i]) return new int[]{i,map.get(potentialMatch)};
+            if(map.containsKey(potentialMatch)) return new int[]{i,map.get(potentialMatch)};
               else map.put(array[i],i);
         }
         return new int[]{};
     }
+
+    // solution two
+
+    public static int[] twoSumSolution2(int[] array,int target){
+
+        Arrays.sort(array);
+
+        int firstPointer = 0;
+        int lastPointer = array.length-1;
+        while(firstPointer<lastPointer){
+            int sum = array[firstPointer] + array[lastPointer];
+            if(sum==target) return new int[]{array[firstPointer],array[lastPointer]};
+            else if(sum<target) firstPointer++;
+            else lastPointer--;
+        }
+       return new int[]{};
+    }
+
 }
